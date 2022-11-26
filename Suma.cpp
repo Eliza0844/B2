a8684170cdc79e7f1aa6d7bb9d5e4ca6a5d6f800
Perlaza Elizabeth <114241026+Eliza0844@users.noreply.github.com>
2022-11-25 21:51:39 -0500

    Create Suma.cpp

---
+++ Suma.cpp
@@ -0,0 +1,18 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	float EPM_x,EPM_i=0,EMP_s=0,EPM_l;
+	cout<<"Ingrese el valor de l: ";
+	cin>>EPM_l;
+	do{
+		cout<<"Ingrese el valor de x: ";
+		cin>>EPM_x;
+		EPM_i=EPM_i+1;
+		EPM_s=EPM_s+EPM_x
+			C_x;
+	}while(EPM_i<EPM_l);
+	cout<<"La suma de los numeros fue: "<<EPM_s<<endl;
+	return 0;
+}
